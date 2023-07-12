4296e60fb6b1bdf2bf68470752423286d5fea085
lawalwendy <wendylawal19@gmail.com>
2023-07-12 17:03:57 +0100

    react js

---
+++ myform/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+// import form from './Component/Form.jsx'
+import BasicExample from './Components/form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SIGN UP TODAY</h1>
+      <BasicExample />
+      <designForm />
+      
+    
+    </div>
+  );
+}
+
+export default App;
